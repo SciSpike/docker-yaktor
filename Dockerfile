d659a938606f9644ff3ad638879f780dbf34f3d9
JK <jonathan.kamke@gmail.com>
2016-06-24 13:51:58 -0700

    necessary changes to switch to alpine

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,17 @@
-FROM node:4
-
-# essential invariants
-ENV PATH ./node_modules/.bin:$PATH
-RUN apt-get update && \
-     apt-get install -y openjdk-7-jre-headless
-RUN apt-get install -y sudo
-
+FROM alpine:3.4
+RUN set -x \
+  && apk add --no-cache \
+    git  openjdk7-jre-base ruby-dev=2.3.1-r0 ruby=2.3.1-r0 nodejs-lts python make g++ ghostscript-fonts graphviz bash sudo python make g++
 ARG VERSION
 RUN npm install -g yaktor@${VERSION:-'latest'} || echo "wait"
 
+RUN /usr/bin/gem install -N asciidoctor-diagram json
 # app & user specifics
-RUN useradd -ms /bin/bash --uid 1000  yaktor 
-RUN adduser yaktor sudo
+RUN adduser -D -u 1000  yaktor
 RUN echo "yaktor ALL=(ALL) NOPASSWD: ALL"  >> /etc/sudoers
 USER yaktor
+RUN echo 'export PATH="./node_modules/.bin:$PATH"' >> ~/.profile
+
 
 VOLUME /app
 WORKDIR /app
--- yaktor.sh
+++ yaktor.sh
@@ -2,26 +2,31 @@
 
 # Ensures current user is the owner of /app's files
 APP_UID=$(id -u)
-term=$(tty)
+term=/dev/console
 FILE_OWNER_UID=$(stat -c '%u' .)
 USER=$(id -un)
 if [ ! $APP_UID == $FILE_OWNER_UID ]; then
+  env | awk '{print "export " $0}' >> ~/.env
   echo "Changing ${USER}'s uid from $APP_UID to $FILE_OWNER_UID"
-  export >> ~/.bashrc
   #do stuff as root
   exec sudo su <<EOL
     #change user id
-    usermod -u $FILE_OWNER_UID $USER
-    #give me bach my terminal
-    chown yaktor $term
+    sed -i.b s/yaktor:x:$APP_UID/yaktor:x:$FILE_OWNER_UID/g /etc/passwd
+    #give me back my terminal or give me null
+    ls $term && chown yaktor $term || ln -s /dev/null $term
+    #give me back my home
+    chown -R yaktor /home/yaktor
     #do stuff as $USER
     exec su - $USER <<LOE
       #go backto $PWD
       cd $PWD
-      #do what every I say with my terminal 
+      #do what every I say with my terminal
+      . ~/.env
+      . ~/.profile
       exec ${@-bash} <$term
 LOE
 EOL
   
 fi
+. ~/.profile
 exec ${@-bash}
